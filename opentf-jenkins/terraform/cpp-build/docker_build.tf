0c4adcf89c12664a4ef0a08b119e1643507a8a43
TheRealGLH <10632069+TheRealGLH@users.noreply.github.com>
2025-02-25 13:48:29 +0100

    fixes project directory for cpp build

--- opentf-jenkins/terraform/cpp-build/docker_build.tf
+++ opentf-jenkins/terraform/cpp-build/docker_build.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 variable "docker_build_host_mount" {
-    default =   "/home/martijn/jenkins/workspace/S8-Terraform"
+    default =   "/home/martijn/jenkins/workspace/S8-Terraform/project"
     type = string
 }
 
